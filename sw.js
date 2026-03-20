8a82bb6a1cc01afbb399d07c0ae5cd8c31a76a74
lesaint99 <tblackston@gmail.com>
2026-03-20 15:38:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'italiano-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'italiano-v3';
+const ASSETS = ['/italiano-vocab/', '/italiano-vocab/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
